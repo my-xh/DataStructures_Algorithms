57c55a712b20f1d8bf44c421e8a5de4bbf0be002
xuheng <2435128850@qq.com>
2021-08-14 14:01:02 +0800

    重命名链表模块

--- pythonds/basic/__init__.py
+++ pythonds/basic/__init__.py
@@ -2,7 +2,7 @@
 
 from pythonds.basic.stack import Stack
 from pythonds.basic.queue import Queue, Deque
-from pythonds.basic.list import UnorderedList, OrderedList
+from pythonds.basic.linked_list import UnorderedList, OrderedList
 from pythonds.basic.hash_table import HashTable
 from pythonds.basic.array_list import ArrayList
 
